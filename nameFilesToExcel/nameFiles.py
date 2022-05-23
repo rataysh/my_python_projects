3c78e7cfbb68da68f8859fe7cf5cad707beeff38
Rataysh <kiril.ebarev@gmail.com>
2022-05-23 16:16:28 +0300

    update 1.01

--- nameFilesToExcel/nameFiles.py
+++ nameFilesToExcel/nameFiles.py
@@ -1,5 +1,4 @@
-import os, time, xlsxwriter, openpyxl
-import pandas as pd
+import os
 from openpyxl import Workbook
 
 
@@ -8,8 +7,7 @@ path_to_directory = input(f'Введите путь к папке начала �
 filename = 'name_all_files_in_directory.xlsx'
 
 list_name_directiry = {}
-delList = []
-testList = []
+listDir = []
 row = 1
 wb = Workbook()
 sheet = wb.active
@@ -17,34 +15,41 @@ sheet2 = wb.create_sheet("nameWithoutEx")
 sheet.title = 'fullName'
 sheet['A' + str(row)] = 'dirs'
 sheet['B' + str(row)] = 'files'
+sheet['C' + str(row)] = 'size'
 sheet2['A' + str(row)] = 'dirs'
 sheet2['B' + str(row)] = 'files'
+sheet2['C' + str(row)] = 'size'
 
 
-def searchDir(path):
+
+def createLidtDir(path):
     for adress, dirs, files in os.walk(path):
-        list_name_directiry[adress.split('\\')[-1]] = files
+    ##Создаем масив с директориями
+        for dir in dirs:
+            listDir.append(f"{adress}\\{dir}")
 
 
-def createListDel(dictName):
-    for key, value in dictName.items():
-        if value == []:
-            delList.append(key)
+def nameAndSizeForEachDir(path):
+    ## Для каждой директории создаю список словарей {Имя: размер}
+    for adress, dirs, files in os.walk(path):
+        if len(files) > 0:
+            list_name_directiry[adress.split('\\')[-1]] = [{file: os.stat(f'{adress}\\{file}').st_size} for file in files]
+
 
 def main():
-    searchDir(path_to_directory)
-    createListDel(list_name_directiry)
+    createLidtDir(path_to_directory)
+    for dir in listDir:
+        nameAndSizeForEachDir(dir)
     row = 1
-    for key in delList:
-        del list_name_directiry[key]
     # С расширением файлов и без
     for key, values in list_name_directiry.items():
-        sheet['A' + str(int(row+1))] = key
-        sheet2['A' + str(int(row + 1))] = key
+        sheet['A' + str(int(row+1))], sheet2['A' + str(int(row + 1))] = key, key
         for i in range(0, len(values)):
             row += 1
-            sheet['B' + str(row)] = values[i]
-            sheet2['B' + str(row)] = '.'.join(values[i].split('.')[0:-1])
+            for k, v in values[i].items():
+                sheet['B' + str(row)], sheet2['B' + str(row)] = k, '.'.join(k.split('.')[0:-1])
+                sheet['C' + str(row)], sheet2['C' + str(row)] = str(round(v / (1024), 2))+ ' Kb', str(round(v / (1024), 2)) + ' Kb'
+
     wb.save(filename)
 
 
