e2872005aa25d32039cc29bc5384d841e4f97475
Rataysh <kiril.ebarev@gmail.com>
2022-05-25 20:08:05 +0300

    update version 1.02: - Fixed bug with identical directory names

--- nameFilesToExcel/nameFiles.py
+++ nameFilesToExcel/nameFiles.py
@@ -1,13 +1,11 @@
 import os
 from openpyxl import Workbook
 
-
 names_xls = []
-path_to_directory = input(f'Введите путь к папке начала сбора наименований:')
+path_to_directory = input(f'Enter the start directory:')
 filename = 'name_all_files_in_directory.xlsx'
 
 list_name_directiry = {}
-listDir = []
 row = 1
 wb = Workbook()
 sheet = wb.active
@@ -21,34 +19,32 @@ sheet2['B' + str(row)] = 'files'
 sheet2['C' + str(row)] = 'size'
 
 
-
-def createLidtDir(path):
+def createListDir(path):
     for adress, dirs, files in os.walk(path):
-    ##Создаем масив с директориями
-        for dir in dirs:
-            listDir.append(f"{adress}\\{dir}")
-
-
-def nameAndSizeForEachDir(path):
-    ## Для каждой директории создаю список словарей {Имя: размер}
-    for adress, dirs, files in os.walk(path):
-        if len(files) > 0:
-            list_name_directiry[adress.split('\\')[-1]] = [{file: os.stat(f'{adress}\\{file}').st_size} for file in files]
+        if files != []:
+            list_name_directiry[adress] = files
 
 
 def main():
-    createLidtDir(path_to_directory)
-    for dir in listDir:
-        nameAndSizeForEachDir(dir)
-    row = 1
-    # С расширением файлов и без
+    createListDir(path_to_directory)
+    ## добавляю полный путь к каждому файлу
     for key, values in list_name_directiry.items():
-        sheet['A' + str(int(row+1))], sheet2['A' + str(int(row + 1))] = key, key
+        for i in range(0, len(values)):
+            list_name_directiry[key][i] = key + '\\' + values[i]
+    ## добавляю размер к каждому файлу
+    for key, values in list_name_directiry.items():
+        for i in range(0, len(values)):
+            list_name_directiry[key][i] = [values[i], os.stat(values[i]).st_size]
+    ## go to excel
+    row = 1
+    for key, values in list_name_directiry.items():
+        sheet['A' + str(int(row + 1))], sheet2['A' + str(int(row + 1))] = key.split('\\')[-1], key.split('\\')[-1]
         for i in range(0, len(values)):
             row += 1
-            for k, v in values[i].items():
-                sheet['B' + str(row)], sheet2['B' + str(row)] = k, '.'.join(k.split('.')[0:-1])
-                sheet['C' + str(row)], sheet2['C' + str(row)] = str(round(v / (1024), 2))+ ' Kb', str(round(v / (1024), 2)) + ' Kb'
+            sheet['B' + str(row)], sheet2['B' + str(row)] \
+                = values[i][0].split('\\')[-1], ''.join(values[i][0].split('\\')[-1].split('.')[:-1])
+            sheet['C' + str(row)], sheet2['C' + str(row)] = str(round(values[i][1] / (1024), 2)) + ' Kb', str(
+            round(values[i][1] / (1024), 2)) + ' Kb'
 
     wb.save(filename)
 
